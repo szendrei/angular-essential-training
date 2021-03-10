e90bb5ce7e1242489d84ce9f08e463ce67196f55
szendrei <szendrgigi@gmail.com>
2021-03-10 20:58:07 +0100

    Lesson 5
    Continue tomorrow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'mw-app',
-    template: '<h1>My App</h1>'
+    templateUrl: './app.component.html',
+    styleUrls: [
+        './app.component.css'
+    ]
 })
 export class AppComponent {}
